30d5a48d4ba5a2bbe76f9d171dbf49a0a6b95ad8
Alexander Elzenaar <aelz176@aucklanduni.ac.nz>
2021-10-03 12:55:56 +1300

    Add timing logging to cusp_plotter.py

--- slice_plotter.py
+++ slice_plotter.py
@@ -1,11 +1,21 @@
 import numpy as np
 import riley
 import matplotlib.pyplot as plt
+from datetime import datetime
 
-ls = riley.riley_slice(np.inf,np.inf,100) # See documentation for this function in riley.py.
+maxdenom = 600
 
-plt.scatter([np.real(t) for t in ls],[np.imag(t) for t in ls],marker=".",s=5,linewidths=0,c='k')
+print(f'Began run at {datetime.now()}')
+ls = riley.riley_slice(np.inf,np.inf,maxdenom) # See documentation for this function in riley.py.
+
+count = len(ls)
+print(f'Finished computing points at {datetime.now()}')
+print(f'Plotting {count} points.')
+
+plt.scatter([np.real(t) for t in ls],[np.imag(t) for t in ls],marker=".",s=5, alpha=0.1,linewidths=0,c='k')
 plt.axis('equal')
 plt.axis([-4,4,-3,3])
 plt.tight_layout()
+print(f'Finished plotting points at {datetime.now()}')
+plt.savefig(f'rileyslice{maxdenom}.png',dpi=2000)
 plt.show()
